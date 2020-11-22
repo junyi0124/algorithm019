e377ddc64c8b6f00d722a97247176bd915d9d36c
junyi.zhang <junyi.zhang@pcitc.com>
2020-11-23 00:25:36 +0800

    w3

--- Week_03/236.二叉树的最近公共祖先.java
+++ Week_03/236.二叉树的最近公共祖先.java
@@ -15,8 +15,30 @@
  * }
  */
 class Solution {
+    
+    TreeNode ans = null;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+
+    /*
+     *递归
+     */
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) 
+            return false;
+        // 探测左子树
+        boolean lson = dfs(root.left,  p, q);
+        // 探测右子树
+        boolean rson = dfs(root.right, p, q);
+        // check
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
         
+        return lson || rson || (root.val == p.val || root.val == q.val);
     }
 }
 // @lc code=end
