082a78631d7fd9fecdf9d555b0fd10da405bce03
junyi.zhang <junyi.zhang@pcitc.com>
2020-12-21 01:53:23 +0800

    week 7

--- Week_07/l208/l208.java
+++ Week_07/l208/l208.java
@@ -7,36 +7,35 @@ package l208;
  */
 
 // @lc code=start
-class TrieNode {
-    // links to node children
-    private TrieNode[] links;
 
-    private final int R = 26;
-
-    private boolean isEnd;
-
-    public TrieNode() {
-        links = new TrieNode[R];
-    }
-
-    public boolean containsKey(char ch) {
-        return links[ch -'a'] != null;
-    }
-    public TrieNode get(char ch) {
-        return links[ch -'a'];
-    }
-    public void put(char ch, TrieNode node) {
-        links[ch -'a'] = node;
-    }
-    public void setEnd() {
-        isEnd = true;
-    }
-    public boolean isEnd() {
-        return isEnd;
-    }
-}
 
 class Trie {
+    class TrieNode {
+        // links to node children
+        private TrieNode[] links;
+        private final int R = 26;
+        private boolean isEnd;
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char ch) {
+            return links[ch -'a'] != null;
+        }
+        public TrieNode get(char ch) {
+            return links[ch -'a'];
+        }
+        public void put(char ch, TrieNode node) {
+            links[ch -'a'] = node;
+        }
+        public void setEnd() {
+            isEnd = true;
+        }
+        public boolean isEnd() {
+            return isEnd;
+        }
+    }
+
     private TrieNode root;
 
     /** Initialize your data structure here. */
