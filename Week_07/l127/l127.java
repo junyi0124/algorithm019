e69957f33392cf1e944184178bce442127399b98
zhangjunyi <zhangjunyi@ultrapower.com.cn>
2020-12-27 14:02:25 +0800

    week07

---
+++ Week_07/l127/l127.java
@@ -0,0 +1,17 @@
+package l127;
+
+/*
+ * @lc app=leetcode.cn id=127 lang=java
+ *
+ * [127] 单词接龙
+ */
+
+import java.util.List;
+
+// @lc code=start
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+    }
+}
+// @lc code=end
---
+++ Week_07/l212/l212.java
@@ -0,0 +1,18 @@
+package l212;
+
+/*
+ * @lc app=leetcode.cn id=212 lang=java
+ *
+ * [212] 单词搜索 II
+ */
+
+import java.util.List;
+
+// @lc code=start
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+
+    }
+}
+// @lc code=end
+
